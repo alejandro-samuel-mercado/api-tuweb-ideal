91f891945dc012f484f5276d2907b4f0c788db54
ale <42730179@fi.unju.edu.ar>
2026-01-26 00:44:26 -0300

    fix endpoint

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -3,22 +3,16 @@ const router = express.Router();
 const contentController = require("../controllers/contentController");
 
 router.get("/plans", contentController.getPlans);
+// Example Projects Routes
 router.get("/example-projects", contentController.getExampleProjects);
-
-router.post("/plans", contentController.createPlan);
-router.put("/plans/:id", contentController.updatePlan);
-router.delete("/plans/:id", contentController.deletePlan);
-
-router.post(
-  "/example-projects",
-
-  contentController.createExampleProject
-);
+router.post("/example-projects", contentController.createExampleProject);
 router.put("/example-projects/:id", contentController.updateExampleProject);
-router.delete(
-  "/example-projects/:id",
+router.delete("/example-projects/:id", contentController.deleteExampleProject);
 
-  contentController.deleteExampleProject
-);
+// Alias for frontend compatibility
+router.get("/projects", contentController.getExampleProjects);
+router.post("/projects", contentController.createExampleProject);
+router.put("/projects/:id", contentController.updateExampleProject);
+router.delete("/projects/:id", contentController.deleteExampleProject);
 
 module.exports = router;
